d9b8ae9856b685fbca6ba2ea95245f2e491f75e0
Davi Goncalves <davigovetmj@gmail.com>
2026-09-14 12:25:04 -0300

    Organiza cards do VIP Manual em duas linhas

--- src/app/vip-manual/page.tsx
+++ src/app/vip-manual/page.tsx
@@ -824,7 +824,7 @@ export default async function VipManualPage({
             RESUMO
         ====================================== */}
 
-        <div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-4 xl:grid-cols-8">
+        <div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4">
 
 
           <Link
